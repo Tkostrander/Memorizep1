a5e8c33b6f5833c07298bf646b661db79c0e2ba6
Thomas Ostrander <tkostrander@icloud.com>
2023-10-29 17:22:42 -0500

    1st commit

--- Memorizep1/ContentView.swift
+++ Memorizep1/ContentView.swift
@@ -7,18 +7,106 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State var emojis = ["👻","🎃","💀","👺","🕸️","🐮","🐷","🐻", "👻","🎃","💀","👺","🕸️","🐮","🐷","🐻"]
+    
+    var transportation = [""]
+    
+    var food = [""]
+    
+    @State var cardCount = 16
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                    ForEach(0..<cardCount, id: \.self) { index in
+                        CardView(content: emojis[index])
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.orange)
+            }
+            HStack {
+                VStack {
+                    Image(systemName: "moonphase.full.moon")
+                        .font(.largeTitle)
+                    Button("Halloween") {
+                        
+                    }
+                }
+                VStack {
+                    Image(systemName: "moonphase.full.moon")
+                        .font(.largeTitle)
+                    Button("Transportation") {
+                        
+                    }
+                }
+                VStack {
+                    Image(systemName: "moonphase.first.quarter")
+                        .font(.largeTitle)
+                    Button("Food") {
+                        
+                    }
+                }
+                
+            }
+            .padding()
         }
         .padding()
     }
+    
 }
 
+struct CardView: View {
+    
+    @State var isFaceUp = true
+    let content: String
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+                    
+                    
+    
+    
+ 
+    
+    
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 #Preview {
     ContentView()
 }
