80aeceb10edca42f6f295e0230f34fcc193f7e84
Thomas Ostrander <tkostrander@icloud.com>
2023-10-28 19:48:37 -0500

    Initial Commit

---
+++ Memorizep1/Memorizep1App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorizep1App.swift
+//  Memorizep1
+//
+//  Created by Thomas Ostrander on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Memorizep1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
